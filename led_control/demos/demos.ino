ee45000ee5ce13c0397d612b5b81df79f24ca41c
Roo Kosherbay <rustamkosherbay@gmail.com>
2018-06-18 01:01:12 -0500

    add reading switch values and changing colors triggered by jumping

--- led_control/demos/demos.ino
+++ led_control/demos/demos.ino
@@ -1,10 +1,12 @@
 #include <FastLED.h>
  
 #define LED_PIN     5
-#define NUM_LEDS    10
+#define NUM_LEDS    30
 #define BRIGHTNESS  64
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
+#define SWITCH_IN_PIN 35
+#define SWITCH_OUT_PIN 53
 CRGB leds[NUM_LEDS];
 
 #define UPDATES_PER_SECOND 100
@@ -26,20 +28,48 @@ CRGB leds[NUM_LEDS];
 // Some notes on the more abstract 'theory and practice' of
 // FastLED compact palettes are at the bottom of this file.
 
+int SWITCH_STATE = 0;
+
 void setup() {
     delay( 3000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
+    pinMode(SWITCH_IN_PIN, INPUT);
+    pinMode(SWITCH_OUT_PIN, OUTPUT);
+    digitalWrite(SWITCH_OUT_PIN, HIGH);
+    Serial.begin(9600);
 }
 
 
 void loop()
 {
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB::Red;
+    if(read_jumps()){
+      int red = random(0,255);
+      int green = random(0,255);
+      int blue = random(0,255);
+      for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            //leds[dot] = CRGB::Red;
+             leds[dot].setRGB( red, green, blue);
             FastLED.show();
             // clear this led for the next time around the loop
             //leds[dot] = CRGB::Black;
-            delay(400);
+      }
+      while(read_jumps()){
+          delay(10);
+        }
     }
+    else{
+      while(!read_jumps()){
+          delay(10);
+        }
+   }
+   delay(10);
 }
+
+bool read_jumps(){
+    SWITCH_STATE = digitalRead(SWITCH_IN_PIN);
+    if(SWITCH_STATE){
+      return SWITCH_STATE;
+    }
+    return SWITCH_STATE;
+  }
