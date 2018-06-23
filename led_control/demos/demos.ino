24a329f0a15285bc25acb3b30e9c94c247f83635
Roo Kosherbay <rustamkosherbay@gmail.com>
2018-06-23 11:38:44 -0500

    remove unnessesary comments, remove update rate

--- led_control/demos/demos.ino
+++ led_control/demos/demos.ino
@@ -9,24 +9,7 @@
 #define SWITCH_OUT_PIN 53
 CRGB leds[NUM_LEDS];
 
-#define UPDATES_PER_SECOND 100
 
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
 CRGB rainbow[7] = {CRGB::Red,CRGB::Green,CRGB::Orange,CRGB::Purple,CRGB::Yellow,CRGB::Blue};
 
 int SWITCH_STATE = 0;
