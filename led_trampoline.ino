7478171a801363a3985eee7dc6e82efa3207e674
Roo Kosherbay <rustamkosherbay@gmail.com>
2018-06-24 12:49:36 -0500

    rename the main code, delete useless code

--- led_trampoline.ino
+++ led_trampoline.ino
@@ -1,21 +1,51 @@
-const int inPin = 35;
-const int outPin = 53;
+#include <FastLED.h>
+ 
+#define LED_PIN     5
+#define NUM_LEDS    30
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define SWITCH_IN_PIN 35
+#define SWITCH_OUT_PIN 53
+CRGB leds[NUM_LEDS];
+CRGB rainbow[7] = {CRGB::Red,CRGB::Green,CRGB::Orange,CRGB::Purple,CRGB::Yellow,CRGB::Blue};
 
-int jumpleverState = 0;
+int SWITCH_STATE = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(inPin, INPUT);
-  pinMode(outPin, OUTPUT);
-  digitalWrite(outPin, HIGH);
-  Serial.begin(9600);
+    delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    pinMode(SWITCH_IN_PIN, INPUT);
+    pinMode(SWITCH_OUT_PIN, OUTPUT);
+    digitalWrite(SWITCH_OUT_PIN, HIGH);
+    Serial.begin(9600);
+}
+int count = 0;
+
+void loop()
+{
+    if(read_jumps()){
+      for(int dot = 0; dot < NUM_LEDS; dot++) {
+        leds[dot] = rainbow[count];
+      }
+      FastLED.show();
+      while(read_jumps()){
+        delay(10);
+        }
+    }
+    else{
+      while(!read_jumps()){
+        delay(10);
+        }
+    }
+    delay(10);
 }
 
-void loop() {
-  digitalWrite(outPin, HIGH);
-  jumpleverState = digitalRead(inPin);
-  Serial.print(" lever = ");
-  Serial.println(jumpleverState);
-
-  delay(2);
-}
+bool read_jumps(){
+    SWITCH_STATE = digitalRead(SWITCH_IN_PIN);
+    if(SWITCH_STATE){
+      return SWITCH_STATE;
+    }
+    return SWITCH_STATE;
+  }
