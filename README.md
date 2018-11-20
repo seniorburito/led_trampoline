b4038cd94f9fafb18107ccf570b34c2de97102a9
Roo Kosherbay <kosher1@stolaf.edu>
2018-11-19 23:37:06 -0600

    clean up the files

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# led_trampoline
+# Led trampoline
 
 ![Jumping on the tramp](../assets/jumping.gif?raw=true)
+
+Checkout my instructable where I explain everything step-by-step [here](https://www.instructables.com/id/Interactive-Neopixel-Trampoline/)
+
+The LED strip has to be wired the following way: 
+
+![Schematic](../assets/Schematic.png?raw=true)
+
+As you wire the LEDs, check that they are working using the check_leds code
+
+Once everything is wired use the code in the main folder 
+
+Features to be added:
+
+More patterns
+
+Sound
+
+Calorie count and display
---
+++ check leds/check_leds.ino
@@ -0,0 +1,29 @@
+// Use this code to check that your leds are working
+// The loop lights up one led at a time so if there is a weird one
+// you can find it 
+
+#include <FastLED.h>
+
+#define LED_PIN     5     // the digital pin that is connected to the strip
+#define NUM_LEDS    30    // here you enter how many leds you have 
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS]; 
+
+void setup() {
+  delay( 3000 ); // power-up safety delay
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+}
+
+// change the delay to make it faster or slower
+// also leds[dot] = CRGB::Blue; can be changed to any color you want
+
+void loop() {
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+    leds[dot] = CRGB::Blue;
+    FastLED.show();
+    delay(200);
+  }
+}
